86f72d7f4dad010178383e2a75e0c2644de2797f
Francisco Paiva <61988754+Francisco-Paiva@users.noreply.github.com>
2021-10-16 12:46:15 +0100

    Upadate IA

---
+++ IA/Fichas Praticas/Ficha 1/ficha1_BB.pl
@@ -0,0 +1,47 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Inteligência Artificial MIEI /3  LEI/3
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Base de Conhecimento com informacao genealogica.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado filho: Filho,Pai -> {V,F}
+
+filho( joao,jose ).
+filho( jose,manuel ).
+filho( carlos,jose ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado pai: Pai,Filho -> {V,F}
+
+pai( P,F ) :- filho( F,P ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado avo: Avo,Neto -> {V,F}
+
+
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado bisavo: Bisavo,Bisneto -> {V,F}
+
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado descendente: Descendente,Ascendente -> {V,F}
+
+
+
+
+
+
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado descendente: Descendente,Ascendente,Grau -> {V,F}
+
+
+
+
+
